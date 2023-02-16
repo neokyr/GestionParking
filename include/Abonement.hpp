315390d0958236ba1f64f2c712a1eea82257bf20
JBaptisteC <120176230+jbaptistec@users.noreply.github.com>
2023-02-16 15:01:00 +0100

    start of coding by JBC

--- include/Abonement.hpp
+++ include/Abonement.hpp
@@ -3,14 +3,26 @@
 #ifndef DEF_ABONEMENT
 #define DEF_ABONEMENT
 
+#include <ctime>
 
-class Abonement
-{
-	private :
-		time_t dateDebutValidite;
-		time_t dateFinValidite;
-		
-	
+
+class Abonement {
+private :
+    time_t dateDebutValidite_;
+    time_t dateFinValidite_;
+
+public:
+    Abonement();
+    ~Abonement();
+    Abonement(const Abonement& cA);
+
+
+    time_t getDateDebutValidite() const;
+    void setDateDebutValidite(time_t dateDebutValidite);
+    time_t getDateFinValidite() const;
+    void setDateFinValidite(time_t dateFinValidite);
+
+
 };
 
 
--- src/Client.cpp
+++ src/Client.cpp
@@ -2,3 +2,20 @@
 /* Generated from GenMyModel */
 
 #include "Client.hpp"
+Client::Client(std::string name, std::string surname, int id, std::string refC) {
+
+}
+
+Client::Client(const Client &cC) {
+
+}
+
+const std::string &Client::getRefClient() const {
+    return refClient_;
+}
+
+void Client::setRefClient(const std::string &refC) {
+    refClient_ = refC;
+}
+
+
